3a2be18b949a8fb9976c7df20c24d88cc43f8b3a
Max Horbul <max@gorbul.net>
2013-10-05 11:40:27 -0700

    implement Mailgun::Event#next method

--- lib/mailgun/event.rb
+++ lib/mailgun/event.rb
@@ -1,18 +1,43 @@
 module Mailgun
   class Event
 
+    attr_reader :next_url, :previous_url
+
     def initialize(mailgun, domain)
       @mailgun = mailgun
       @domain  = domain
     end
 
     def find(params)
-      Mailgun.submit :get, events_url, params
+      params = parse_params(params)
+      response = Mailgun.submit :get, events_url, params
+      parse_response(response)
+      response["items"]
+    end
+
+    def next
+      response = Mailgun.submit :get, next_url
+      parse_response(response)
+      response["items"]
     end
 
     private
+    def parse_response(response)
+      @next_url = response['paging']['next']
+      @previous_url = response['paging']['next']
+    end
+
     def events_url
       "#{@mailgun.base_url}#{@domain}/events"
+    end
+
+    def parse_params(params)
+      [:begin, :end].each do |key|
+        if params[key]
+          params[key] = Time.parse(params[key].to_s).rfc2822
+        end
+      end
+      params
     end
 
   end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -3,12 +3,21 @@ require 'spec_helper'
 describe Mailgun::Event do
 
   let(:mailgun) { Mailgun({:api_key => "api-key" }) }
-  let(:events_url) { "http://mailgun.com/events/url" }
+  let(:events_url) { "https://example.mailgun.org/domain.com/events" }
   let(:event) { Mailgun::Event.new(mailgun, "domain.com") }
 
+  let(:result) { JSON.parse(File.read(File.join(File.dirname(__FILE__), 'fixtures/events.json'))) }
+  let(:last_result) do
+    last_result = result.dup
+    last_result["items"] = []
+    last_result
+  end
+
   before do
-    event.stub(:events_url =>  events_url)
+    mailgun.stub(:base_url => "https://example.mailgun.org/")
+    event.stub(:mailgun =>  mailgun)
     Mailgun::Event.should_receive(:new).and_return(event)
+    Mailgun.stub(:submit).and_return(result)
   end
 
   it "should have events" do
@@ -20,6 +29,32 @@ describe Mailgun::Event do
     it "should make a GET request with correct params" do
       params = {:a => :b, :c => :d}
       Mailgun.should_receive(:submit).with(:get, events_url, params)
+      mailgun.events.find(params)
+    end
+
+    it "should be able to fetch next page" do
+      mailgun.events.find({})
+      Mailgun.should_receive(:submit).with(:get, result['paging']['next'])
+      mailgun.events.next
+    end
+
+    it "should be able to fetch one more next page" do
+      mailgun.events.next
+      Mailgun.should_receive(:submit).with(:get, result['paging']['next'])
+      mailgun.events.next
+    end
+
+    it "should convert begin & end dates to RFC2822 if they are Time objects" do
+      now = Time.now
+      params = { :begin => now, :end => now + 100, :a => :b }
+      Mailgun.should_receive(:submit).with(:get, events_url, {:begin => now.rfc2822, :end => (now + 100).rfc2822, :a => :b})
+      mailgun.events.find(params)
+    end
+
+    it "should convert begin & end dates to RFC2822 if they are strings " do
+      now = Time.now
+      params = { :begin => now.to_s, :end => (now + 100), :a => :b }
+      Mailgun.should_receive(:submit).with(:get, events_url, {:begin => now.rfc2822, :end => (now + 100).rfc2822, :a => :b})
       mailgun.events.find(params)
     end
 
